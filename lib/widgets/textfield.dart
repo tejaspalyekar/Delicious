8bd70f01bbe1994c4bb8601e26cb2a30ca130420
Tejas Vivek Palyekar <tejaspalyekar18@gmail.com>
2023-07-20 04:15:22 +0530

    commit

---
+++ lib/widgets/textfield.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class ReuseabletextField extends StatefulWidget {
+  ReuseabletextField(
+      {super.key,
+      required this.text,
+      required this.icon,
+      required this.isPasswordType,
+      required this.controller});
+  String text;
+  IconData icon;
+  bool isPasswordType;
+  TextEditingController controller;
+  @override
+  State<ReuseabletextField> createState() => _ReuseabletextFieldState();
+}
+
+class _ReuseabletextFieldState extends State<ReuseabletextField> {
+  bool pass = widget.isPasswordType ? true : false;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: widget.controller,
+      obscureText: pass,
+      enableSuggestions: !widget.isPasswordType,
+      autocorrect: !widget.isPasswordType,
+      cursorColor: Colors.white,
+      style: TextStyle(
+          color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.9)),
+      decoration: InputDecoration(
+        prefixIcon: Icon(widget.icon, color: Colors.white70),
+        suffixIcon: widget.isPasswordType
+            ? IconButton(
+                onPressed: () {
+                  pass = true;
+                  setState(() {});
+                },
+                icon: const Icon(
+                  Icons.remove_red_eye,
+                  color: Colors.white,
+                ))
+            : const Text(""),
+        labelText: widget.text,
+        labelStyle: TextStyle(
+            color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.9)),
+        filled: true,
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        fillColor: const Color.fromARGB(146, 255, 255, 255).withOpacity(0.3),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30.0),
+            borderSide: const BorderSide(width: 0, style: BorderStyle.none)),
+      ),
+      keyboardType: widget.isPasswordType
+          ? TextInputType.visiblePassword
+          : TextInputType.emailAddress,
+    );
+  }
+}
+
+
+
+/*TextField reuseableTextField(String text, IconData icon, bool isPasswordType,
+    TextEditingController controller) {
+  return TextField(
+    controller: controller,
+    obscureText: isPasswordType,
+    enableSuggestions: !isPasswordType,
+    autocorrect: !isPasswordType,
+    cursorColor: Colors.white,
+    style: TextStyle(
+        color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.9)),
+    decoration: InputDecoration(
+      prefixIcon: Icon(icon, color: Colors.white70),
+      suffixIcon: isPasswordType
+          ? IconButton(
+              onPressed: () {},
+              icon: isPasswordType
+                  ? const Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.white,
+                    )
+                  : const Text(""),
+            )
+          : const Text(""),
+      labelText: text,
+      labelStyle: TextStyle(
+          color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.9)),
+      filled: true,
+      floatingLabelBehavior: FloatingLabelBehavior.never,
+      fillColor: const Color.fromARGB(146, 255, 255, 255).withOpacity(0.3),
+      border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: const BorderSide(width: 0, style: BorderStyle.none)),
+    ),
+    keyboardType: isPasswordType
+        ? TextInputType.visiblePassword
+        : TextInputType.emailAddress,
+  );
+}*/
